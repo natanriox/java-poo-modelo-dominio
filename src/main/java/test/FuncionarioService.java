6616de091badf0f253cbb324951c9d58b9530b06
Natanael Rios <natanaelsrios8@gmail.com>
2026-03-29 12:19:25 -0300

    Update address in FuncionarioService main method

--- src/main/java/test/FuncionarioService.java
+++ src/main/java/test/FuncionarioService.java
@@ -5,7 +5,7 @@ import domain.Funcionario;
 
 public class FuncionarioService {
     public static void main(String[] args) {
-        Endereco address = new Endereco("Rua da caralhada do que se foda", "01-22345");
+        Endereco address = new Endereco("Rua do sol", "0123456-07");
         Funcionario worker = new Funcionario("Atlas", "999.888.777-36", "Atlas@empresa.com", address, 54321, 2000);
 
         System.out.println("----------------------------");
